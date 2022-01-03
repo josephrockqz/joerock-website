137699c38dd7ef9b688183e4a49b298407cd52fd
josephrockqz <josephrockqz@gmail.com>
2022-01-03 17:07:16 -0600

    changed color scheme and landing page

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" absolute temporary app width="150" height="340">
+    <!-- <v-navigation-drawer v-model="drawer" absolute temporary app width="150" height="340">
       <v-list class="pt-4">
-        <v-list-tile active-class="green--text" to="/">
+        <v-list-tile active-class="blue--text" to="/">
           <v-list-tile-content>
             <v-list-tile-title>LANDING</v-list-tile-title>
           </v-list-tile-content>
@@ -23,12 +23,12 @@
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
     <v-toolbar flat dense color="transparent" scroll-off-screen>
       <v-toolbar-side-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline">
         <span class="font-weight-light">Joseph</span>
-        <span class="green--text">Rock</span>
+        <span style="color: #CD7F32">Rock</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn @click="changeTheme" depressed small icon class="hidden-md-and-up">
@@ -36,10 +36,10 @@
         <v-icon v-else>fas fa-moon</v-icon>
       </v-btn>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/" active-class="green--text headline">LANDING</v-btn>
-        <v-btn flat to="/resume" active-class="green--text headline">RESUME</v-btn>
-        <v-btn flat to="/games" active-class="green--text headline">GAMES</v-btn>
-        <v-btn flat to="/contact" active-class="green--text headline">CONTACT</v-btn>
+        <v-btn flat to="/" active-class="blue--text headline">LANDING</v-btn>
+        <v-btn flat to="/resume" active-class="blue--text headline">RESUME</v-btn>
+        <v-btn flat to="/games" active-class="blue--text headline">GAMES</v-btn>
+        <v-btn flat to="/contact" active-class="blue--text headline">CONTACT</v-btn>
         <v-btn @click="changeTheme" depressed small icon>
           <v-icon v-if="goDark==true">fas fa-sun</v-icon>
           <v-icon v-else>fas fa-moon</v-icon>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,29 +1,9 @@
 <template>
   <v-layout column justify-center align-center class="mt-4 pt-2">
-    <VueCompareImage
-      class="hidden-md-and-down"
-      hover
-      :style="{ minWidth: '1300px' }"
-      :sliderLineWidth="sliderLine"
-      :handleSize="hSize"
-      :leftImage="leftImage"
-      :rightImage="rightImage"
-      :sliderPositionPercentage="sliderPosition"
-    />
-    <VueCompareImage
-      class="hidden-lg-and-up"
-      hover
-      :style="{ maxWidth: '400px' }"
-      :sliderLineWidth="sliderLine"
-      :handleSize="hSize"
-      :leftImage="leftImage2"
-      :rightImage="rightImage2"
-      :sliderPositionPercentage="sliderPosition"
-    />
 
-    <br>
+    <img src="../../public/headshot.jpg" style="width: 300px; height: auto; margin: auto; padding: 25px; border-radius: 50%; text-align: center;" class="w3-circle" />
 
-    <vue-typer class="headline" :repeat="0" text="Eldin Zaimovic"></vue-typer>
+    <vue-typer class="headline" :repeat="0" text="Joseph Rock"></vue-typer>
     <vue-typer
       :text="text1"
       :repeat="Infinity"
@@ -37,59 +17,58 @@
       :erase-on-complete="false"
       caret-animation="smooth"
     ></vue-typer>
+
     <br>
+
     <v-flex xs12 sm6 md4 lg4 xl4>
       <v-card flat color="transparent" max-width="500">
         <v-card-title primary-title>
           <div>
             <h3 class="headline mb-0">
-              <span>About</span>
-              <span class="green--text">Me</span>
+              <span>About </span>
+              <span style="color: #CD7F32">Me</span>
             </h3>
             <div>
-              <p>
-                Hello! I’m Eldin Zaimovic. I'm a front-end developer who
-                is passionate about
-                <span
-                  class="green--text font-weight-bold"
-                >VUE</span>, building great user experiences,
-                fighting for simplicity over complexity and constantly learning.
-                When I'm not coding or making videos, you'll find me traveling across Europe.
-              </p>
+                Hi. My name is Joseph Rock. I am currently studying computer science & mathematics at the University of Virginia. I am passionate about
+                software engineering and am hopeful to use my skills to make a positive impact on others. When I'm not programming I am usually at the gym,
+                playing golf, or trying new food.
             </div>
           </div>
         </v-card-title>
 
-        <v-card-actions class="hidden-sm-and-down">
-          <v-btn
-            v-for="icon in icons"
-            :key="icon.icon"
-            fab
-            dark
-            outline
-            color="green"
-            :href="icon.href"
-            target="_blank"
-          >
-            <v-icon dark>{{icon.icon}}</v-icon>
-          </v-btn>
-        </v-card-actions>
+        <v-layout column justify-center align-center>
 
-        <v-card-actions class="hidden-md-and-up justify-center">
-          <v-btn
-            v-for="icon in icons"
-            :key="icon.icon"
-            small
-            fab
-            dark
-            outline
-            color="green"
-            :href="icon.href"
-            target="_blank"
-          >
-            <v-icon dark>{{icon.icon}}</v-icon>
-          </v-btn>
-        </v-card-actions>
+          <v-card-actions class="hidden-sm-and-down">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon.icon"
+              fab
+              dark
+              outline
+              color="blue"
+              :href="icon.href"
+              target="_blank"
+            >
+              <v-icon dark>{{icon.icon}}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <v-card-actions class="hidden-md-and-up justify-center">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon.icon"
+              small
+              fab
+              dark
+              outline
+              color="blue"
+              :href="icon.href"
+              target="_blank"
+            >
+              <v-icon dark>{{icon.icon}}</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-layout>
       </v-card>
     </v-flex>
   </v-layout>
@@ -97,41 +76,26 @@
 
 <script>
 import { VueTyper } from 'vue-typer'
-import VueCompareImage from 'vue-compare-image'
 
 export default {
   components: {
-    'vue-typer': VueTyper,
-    VueCompareImage
+    'vue-typer': VueTyper
   },
   data () {
     return {
       icons: [
-        { href: 'https://github.com/EldinZaimovic', icon: 'fab fa-github' },
+        { href: 'https://github.com/josephrockqz', icon: 'fab fa-github' },
         {
-          href: 'https://stackoverflow.com/users/story/9660700',
-          icon: 'fab fa-stack-overflow'
-        },
-        { href: 'https://twitter.com/EldinMatricar', icon: 'fab fa-twitter' },
-        {
-          href: 'https://www.linkedin.com/in/eldin-zaimovic',
+          href: 'https://www.linkedin.com/in/joseph-rock',
           icon: 'fab fa-linkedin-in'
         },
         {
-          href: 'https://www.xing.com/profile/Eldin_Zaimovic?sc_o=mxb_p',
-          icon: 'fab fa-xing'
-        },
-        {
-          href: 'https://www.facebook.com/EldinVasVoli',
+          href: 'https://www.facebook.com/joseph.rock.372',
           icon: 'fab fa-facebook-f'
         },
-        { href: 'https://www.instagram.com/matricar', icon: 'fab fa-instagram' }
+        { href: 'https://www.instagram.com/joerock00', icon: 'fab fa-instagram' }
       ],
-      text1: ['Front-End Developer', 'Web Developer', 'Web Designer'],
-      leftImage: 'https://i.imgur.com/bU50uS9.jpg',
-      rightImage: 'https://i.imgur.com/z3ZoEEw.jpg',
-      leftImage2: 'https://i.imgur.com/SXBitjh.jpg',
-      rightImage2: 'https://i.imgur.com/Dcz2PGx.jpg',
+      text1: ['Software Engineer', 'Web Developer', 'Front-End Developer'],
       sliderLine: 0,
       hSize: 0,
       sliderPosition: 0.5
@@ -155,20 +119,20 @@ export default {
   font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
 }
 .vue-typer .custom.char.typed {
-  color: green;
+  color: #CD7F32;
 }
 .vue-typer .custom.char.selected {
-  color: #e91e63;
+  color: blue;
 }
 
 .vue-typer .custom.caret {
-  animation: rocking 1s ease-in-out 0s infinite;
+  animation: 1s ease-in-out 0s infinite;
 }
 .vue-typer .custom.caret.typing {
-  background-color: green;
+  background-color: #CD7F32;
 }
 .vue-typer .custom.caret.selecting {
   display: inline-block;
-  background-color: #e91e63;
+  background-color: blue;
 }
 </style>
