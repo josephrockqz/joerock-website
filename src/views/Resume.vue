d14074b0f1fc11334414c384eda55f32a42f5ac4
josephrockqz <josephrockqz@gmail.com>
2022-01-03 17:23:06 -0600

    changed resume page

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -4,41 +4,28 @@
       <h2 class="mb-4 pl-4 pt-2">Education</h2>
       <v-card-text class="py-0">
         <v-timeline align-top dense>
-          <v-timeline-item color="green" small>
+          <v-timeline-item color="#CD7F32" small>
             <v-layout pt-3>
               <v-flex xs4>
-                <div class="caption font-weight-bold green--text">JAN 2018 -</div>
-                <div class="caption font-weight-bold green--text">PRESENT</div>
+                <div class="caption font-weight-bold blue--text">August 2015 -</div>
+                <div class="caption font-weight-bold blue--text">May 2018</div>
               </v-flex>
               <v-flex class="pl-2">
-                <strong>Full Stack Web Development Course</strong>
-                <div class="caption mb-2">freeCodeCamp</div>
+                <strong>Rockford Auburn High School</strong>
+                <div class="caption mb-2">National AP Scholar</div>
               </v-flex>
             </v-layout>
           </v-timeline-item>
 
-          <v-timeline-item color="green" small>
+          <v-timeline-item color="#CD7F32" small>
             <v-layout pt-3>
               <v-flex xs4>
-                <div class="caption font-weight-bold green--text">SEP 2007 -</div>
-                <div class="caption font-weight-bold green--text">MAY 2014</div>
+                <div class="caption font-weight-bold blue--text">August 2018 -</div>
+                <div class="caption font-weight-bold blue--text">Present</div>
               </v-flex>
               <v-flex class="pl-2">
-                <strong>Telecommunications Engineering</strong>
-                <div class="caption mb-2">University of East Sarajevo</div>
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-
-          <v-timeline-item color="green" small>
-            <v-layout pt-3>
-              <v-flex xs4>
-                <div class="caption font-weight-bold green--text">SEP 2003 -</div>
-                <div class="caption font-weight-bold green--text">MAY 2007</div>
-              </v-flex>
-              <v-flex class="pl-2">
-                <strong>Electrical Technician for Computers</strong>
-                <div class="caption mb-2">Electro-technical school Doboj</div>
+                <strong>University of Virginia</strong>
+                <div class="caption mb-2">Computer Science & Mathematics</div>
               </v-flex>
             </v-layout>
           </v-timeline-item>
@@ -49,41 +36,28 @@
       <h2 class="mb-4 pl-4">Experience</h2>
       <v-card-text class="py-0">
         <v-timeline align-top dense>
-          <v-timeline-item color="green" small>
+          <v-timeline-item color="#CD7F32" small>
             <v-layout pt-3>
               <v-flex xs4>
-                <div class="caption font-weight-bold green--text">DEC 2016 -</div>
-                <div class="caption font-weight-bold green--text">PRESENT</div>
+                <div class="caption font-weight-bold blue--text">May 2020 -</div>
+                <div class="caption font-weight-bold blue--text">January 2021</div>
               </v-flex>
               <v-flex class="pl-2">
-                <strong>Freelance Frontend Developer</strong>
-                <div class="caption mb-2">Remote</div>
+                <strong>Web Developer</strong>
+                <div class="caption mb-2">Montel Technologies</div>
               </v-flex>
             </v-layout>
           </v-timeline-item>
 
-          <v-timeline-item color="green" small>
+          <v-timeline-item color="#CD7F32" small>
             <v-layout pt-3>
               <v-flex xs4>
-                <div class="caption font-weight-bold green--text">FEB 2018 -</div>
-                <div class="caption font-weight-bold green--text">JULY 2018</div>
+                <div class="caption font-weight-bold blue--text">June 2021 -</div>
+                <div class="caption font-weight-bold blue--text">August 2021</div>
               </v-flex>
               <v-flex class="pl-2">
-                <strong>Teacher for electrical technicians</strong>
-                <div class="caption mb-2">High School Doboj Istok</div>
-              </v-flex>
-            </v-layout>
-          </v-timeline-item>
-
-          <v-timeline-item color="green" small>
-            <v-layout pt-3>
-              <v-flex xs4>
-                <div class="caption font-weight-bold green--text">AUG 2014 -</div>
-                <div class="caption font-weight-bold green--text">APR 2016</div>
-              </v-flex>
-              <v-flex class="pl-2">
-                <strong>IT Technician</strong>
-                <div class="caption mb-2">MFS-EMMAUS</div>
+                <strong>Technical Intern</strong>
+                <div class="caption mb-2">Capital One</div>
               </v-flex>
             </v-layout>
           </v-timeline-item>
@@ -93,36 +67,36 @@
     <v-layout row justify-center align-center wrap class="mt-4 pt-2">
       <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
         <h2>
-          <span>Design</span>
+          <span>Software</span>
 
-          <span class="green--text">Skills</span>
+          <span style="color: #CD7F32;">Skills</span>
         </h2>
         <br>
-        <strong>Photoshop</strong>
-        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
-        <strong>Adobe XD</strong>
-        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
-        <strong>Zeplin</strong>
-        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Docker</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="blue" height="16"></v-progress-linear>
+        <strong>MongoDB</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="blue" height="16"></v-progress-linear>
+        <strong>Git</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="blue" height="16"></v-progress-linear>
       </v-flex>
       <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
         <h2>
           <span>Coding</span>
-          <span class="green--text">Skills</span>
+          <span style="color: #CD7F32;">Skills</span>
         </h2>
         <br>
-        <strong>VUE/NUXT</strong>
-        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Vue.js/Node.js</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="blue" height="16"></v-progress-linear>
         <strong>HTML/CSS</strong>
-        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="blue" height="16"></v-progress-linear>
         <strong>JavaScript</strong>
-        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="blue  " height="16"></v-progress-linear>
       </v-flex>
       <div class="mt-4 pt-2">
         <v-btn
-          href="https://docs.google.com/document/d/1WSR42FEgUahJuR_RArW1wz7TfUBlgj13zekRidmQgs4/edit?usp=sharing"
+          href="/resume_joseph_rock.pdf"
           target="_blank"
-          color="green"
+          color="#CD7F32"
           dark
           large
         >
@@ -160,11 +134,11 @@ export default {
   methods: {
     startBuffer () {
       this.interval = setInterval(() => {
-        this.buffer1 = 90
-        this.buffer2 = 85
-        this.buffer3 = 80
+        this.buffer1 = 70
+        this.buffer2 = 75
+        this.buffer3 = 65
         this.buffer4 = 80
-        this.buffer5 = 90
+        this.buffer5 = 70
         this.buffer6 = 70
       }, 800)
     }
