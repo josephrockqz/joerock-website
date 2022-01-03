979e20adcb2d1b718d1c5b3fa84a44af4fbb2849
josephrockqz <josephrockqz@gmail.com>
2022-01-03 17:48:43 -0600

    changed some of contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,35 +3,30 @@
     <v-layout row justify-center align-center wrap class="mt-4 pt-2">
       <v-flex xs12 sm12 md6 lg6 xl6>
         <h2 class="pb-4 mt-2">
-          <span>GetIn</span>
-          <span class="green--text">Touch</span>
+          <span>Contact </span>
+          <span style="color: #CD7F32">Info</span>
         </h2>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-map-marker-alt</v-icon>
-          <span>Doboj,&nbsp;</span>
-          <span class="green--text">Bosnia & Herzegovina</span>
+          <v-icon large color="#CD7F32" left>fas fa-map-marker-alt</v-icon>
+          <span>Virginia,&nbsp;</span>
+          <span class="blue--text">United States</span>
         </div>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-envelope</v-icon>
-          <span>eldin@</span>
-          <span class="green--text">zaimovic.com</span>
+          <v-icon large color="#CD7F32" left>fas fa-envelope</v-icon>
+          <span>josephrockqz@</span>
+          <span class="blue--text">gmail.com</span>
         </div>
         <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-phone</v-icon>
-          <span>+387&nbsp;</span>
-          <span class="green--text">61 596 676</span>
-        </div>
-        <div class="py-4 subheading font-weight-bold">
-          <v-icon large color="green" left>fas fa-check</v-icon>
-          <span>Freelance</span>
-          <span class="green--text">Available</span>
+          <v-icon large color="#CD7F32" left>fas fa-check</v-icon>
+          <span>Always </span>
+          <span class="blue--text">Available</span>
         </div>
       </v-flex>
 
       <v-flex xs12 sm12 md6 lg6 xl6>
         <h2 class="pb-4 mb-4">
-          <span>Contact</span>
-          <span class="green--text">Form</span>
+          <span>Contact </span>
+          <span style="color: #CD7F32">Form</span>
         </h2>
 
         <form method="POST" action="https://formspree.io/eldin@zaimovic.com">
